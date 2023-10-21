55d6f9c8a1e086d41b2b7d51439ff5d2b7c70e1c
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-10-21 16:18:52 +0200

    2

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,13 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
+
   socials: {
     twitter: 'deniseschleier',
     github: 'deniskropp/nuxt-kick-start'
+  },
+
+  nuxtIcon: {
+    size: '2em'
   }
 })
