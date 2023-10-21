d15ef2307ecec8c354bd259640555c1059855ce8
Denis Oliver Kropp <dok@directfb1.org>
2023-10-21 23:47:18 +0200

    more structure

--- composables/useChat.ts
+++ composables/useChat.ts
@@ -1,24 +1,73 @@
 import { kickIt } from '../lib/kick'
 
+interface Item {
+    type: string
+    tag?: string
+    value?: string
+    children?: Item[]
+}
+
+function getText(item: Item): string {
+    var str: string[] = []
+
+    if (item.type === 'text')
+        str.push(item.value ?? 'ERROR')
+
+    if (item.tag === 'h1')
+        str.push('# ')
+
+    if (item.tag === 'h2')
+        str.push('## ')
+
+    if (item.tag === 'h3')
+        str.push('### ')
+
+    if (item.children)
+        item.children.forEach(c => str.push(getText(c)))
+
+    return str.join('\n')
+}
+
 export async function useChat(user?: string) {
-    const v = await useAsyncData('kick', () => kickIt('/ai', 'chat', {
-        messages: [
-            {
-                role: 'json',
-                content: JSON.stringify(useContent().page.value.body.children)
-            },
-            {
-                role: user ? 'user' : 'system',
-                content: user ?? 'Your task is to continue the markdown content in plain text format'
-            }
-        ]
+    const { page } = useContent()
+    const { body } = page.value
+
+    console.log('useChat(body)', body)
+
+    const parts = body.children.map((c: any) => ({
+        role: `element:${c.tag ?? ''}`,
+        content: getText(c)
     }))
 
-    if (v.data.value) {
-        const d = v.data.value
+    const chats = body.children.filter((c: any) => c.tag === 'chat')
 
-        return d.type === 'error' ? d.what :
-            d.type === 'chat' ? d.messages[d.messages.length - 1].content : d.type
+    console.log('useChat(chats)', chats)
+
+    const messages = [
+        {
+            role: user ? 'user' : 'system',
+            content: user ?? 'Your task is to generate markdown content as per chat element.'
+            //content: 'Your task is to continue the markdown content in plain text format'
+        },
+        ...Object.entries(chats[0].props).map((p: any) => ({
+            role: `${p[0]}`,
+            content: p[1]
+        })),
+        ...parts
+    ]
+
+    console.log('useChat(messages)', messages)
+
+    const ret = await useAsyncData('kick', () => kickIt('/ai', 'chat', { messages }))
+
+    if (ret.data.value) {
+        const data = ret.data.value
+
+        return (
+            data.type === 'error' ? data.what :
+                data.type === 'chat' ? data.messages[data.messages.length - 1].content :
+                    data.type
+        )
     }
 
     return ''
