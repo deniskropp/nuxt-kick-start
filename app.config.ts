b1736dfbc6d18416516460cd18b2dc392c63b933
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-10-21 16:11:18 +0200

    twitter

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
+    twitter: 'deniseschleier',
     github: 'Atinux/content-wind'
   }
 })
