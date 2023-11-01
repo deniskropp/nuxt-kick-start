061c6f56fd9c59279f91e882f5ffe122b7229a1f
Denis Oliver Kropp <Leichenbegatter@outlook.com>
2023-11-01 14:51:34 +0100

    stuff

---
+++ components/content/Chat2.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'Untitled chat'
+    },
+})
+</script>
+
+<template>
+    <input :value="props.title" />
+</template>
