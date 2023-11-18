f42277a82e6da6cf04c9e248799806f62bb9c12a
Denis Kropp <Leichenbegatter@outlook.com>
2023-11-18 19:13:27 +0000

    ex

--- components/content/TestChat.vue
+++ components/content/TestChat.vue
@@ -11,36 +11,78 @@ const last = ref('')
 
 const { markdown, pending, ask, messages, generate } = await useKick(props)
 
-async function go() {
-    async function run() {
+async function go(append: boolean) {
+
+    const toc = await queryContent({
+        type: 'page',
+    })
+
+    const renderToc = (tree: any[]) => {
+        console.log(tree)
+        let markdown = ''
+
+        for (const node of tree) {
+            if (node.type === 'heading') {
+                if (node.depth === 1) {
+                    markdown += `# ${node.title}\n`
+                } else if (node.depth === 2) {
+                    markdown += `## ${node.title}\n`
+                } else {
+                    markdown += `### ${node.title}\n`
+                }
+            } else {
+                //                markdown += `${node.content}\n`
+                markdown += `${node._path}\n`
+            }
+        }
+
+        return markdown
+    }
+
+    const renderedToc = renderToc(await toc.find())
+
+    async function run(append: boolean) {
+        if (last.value && append)
+            messages.push({ role: 'assistant', content: last.value })
+
         messages.push({ role: 'user', content: input.value })
 
-        last.value = await generate(messages)
+        last.value = await generate([...messages, { role: 'user:paths', content: renderedToc }])
 
-        markdown.value += `\n\n---\n\n/${input.value}/\n---\n\n${last.value}`
-
-        messages.push({ role: 'assistant', content: last.value })
+        if (append)
+            markdown.value += `\n\n---\n\n*${input.value}*\n---\n\n${last.value}`
+        else
+            markdown.value = `\n\n---\n\n*${input.value}*\n---\n\n${last.value}`
     }
 
     pending.value = true
 
-    await run()
+    await run(append)
 
     pending.value = false
 }
 </script>
 
 <template>
-    <div style="width: 40px; height: 2px; background-color: rebeccapurple; margin-top: 1em;" v-if="markdown"></div>
-    <MarkdownStringRenderer :markdownString="markdown" />
+    <div style="display: flex; flex-direction: column;">
+        <div style="width: 40px; height: 2px; background-color: rebeccapurple; margin-top: 1em;" v-if="markdown"></div>
 
-        <input v-if="!pending" v-model="input" style="color: #222222; width: 70%;" />
+        <MarkdownStringRenderer :markdownString="markdown" />
 
-        <button style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em;" @click="() => {go()}" :disabled="pending">
-            <div v-if="!pending">✨</div>
+        <input v-if="!pending" v-model="input" style="color: #222222; width: 100%;" />
+
+        <button v-if="!pending" style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 2em;"
+            @click="() => { go(false) }" :disabled="pending">
+            <div>🏴‍☠️</div>
+        </button>
+
+        <button style="border: 1px solid; text-align: left; margin-top: 1em; padding: 0.5em 1em 0.2em 0.5em; min-height: 7em;"
+            @click="() => { go(true) }" :disabled="pending">
+            <div v-if="!pending" style="scale: 0.9; margin: -10% -5% -5% -5%; border-top: 1px solid;">
+                <div>✨</div>
+                <slot />
+            </div>
             <div v-if="pending">please wait...</div>
-        <div v-if="!pending" style="scale: 0.7; margin: -20% -10% -10% -10%; border-top: 1px solid;">
-            <slot />
-        </div>
-    </button>
+        </button>
+    </div>
 </template>
