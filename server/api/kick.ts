878623d30366b68b6ecfb5053293e76c8c4bdd9a
Denis Kropp <Leichenbegatter@outlook.com>
2024-03-16 18:15:53 +0000

    handle preflight

--- server/api/kick.ts
+++ server/api/kick.ts
@@ -2,6 +2,12 @@ import { defineEventHandler, type EventHandlerRequest, type H3Event } from 'h3'
 import { useCohere, useKickTemplate, type KickTemplate } from '../../modules/kick-it/src'
 
 export default defineEventHandler(async (event: H3Event<EventHandlerRequest>) => {
+    if (event.method === 'OPTIONS') {
+        event.node.res.statusCode = 204;
+        event.node.res.statusMessage = "No Content.";
+        return 'OK';
+    }
+
     const body = await readBody<KickTemplate>(event)
 
     return useCohere(useKickTemplate(body))
